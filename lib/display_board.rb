e3cac2d223bd2f31b16dd3728f65231bd68fcbb4
shawn campbell <shacoca@yahoo.com>
2020-07-26 17:11:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
